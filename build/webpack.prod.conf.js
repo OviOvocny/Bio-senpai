76e15a3aba7f6d01b90c1c20188b9660295eeee4
OviOvocny <werint.pp@gmail.com>
2018-01-25 10:40:54 +0100

    change API base to new domain thanks DOT KT

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -31,7 +31,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env,
-      'API_BASE': JSON.stringify('api.bio-senpai.tk/api')
+      'API_BASE': JSON.stringify('api.bio-senpai.ovi.moe/api')
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
