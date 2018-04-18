37c0aa9a916f1fbf314008506729924bbc3dc481
OviOvocny <werint.pp@gmail.com>
2018-04-18 20:56:05 +0200

    redesign category cards

--- src/components/category-cards.vue
+++ src/components/category-cards.vue
@@ -2,7 +2,7 @@
   <div class="card-categories">
     <router-link replace v-for="cat in categories" :to="`/${page}/${cat.slug}`" :key="cat.slug" class="card-category">
         <icon :symbol="cat.icon" class="card-category__icon"></icon>
-        <div class="card-category__name">{{cat.category}}</div>
+        <span class="card-category__name">{{cat.category}}</span>
     </router-link>
   </div>
 </template>
@@ -23,38 +23,29 @@ export default {
   flex-wrap wrap
 
 .card-category
-  color #1e2430
   text-decoration none
-  background-color white
-  border-radius 15px
+  color white
+  background-color #374258
+  border-radius 2em
   text-align center
-  width 120px
-  margin 1em
-  padding 1em
+  margin .75em
+  padding .5em 1em
+  transition background-color .3s, color.2s
   &:visited
     color @color
-  &:hover .card-category__icon
-    color hsl(150, 80%, 60%)
-  &.router-link-active .card-category__icon
-    color white
+  &:hover
+    background-color lighten(#374258, 20%)
+  &.router-link-active
     background-color hsl(150, 80%, 40%)
 
 .card-category__icon
   display inline-block
-  background-color #1e2430
-  color white
-  font-size 2em
-  vertical-align top
-  text-align center
-  width @font-size
-  height @width
-  padding-top (@height / 8)
-  border-radius @font-size
-  margin-bottom .35em
-  transition background-color .3s, color.2s
+  font-size 1.2em
+  margin-right .5em
 
 .card-category__name
-  font-size 1.1em
+  display inline-block
+  font-size 1em
 
 @media (max-width: 690px)
   .card-category
@@ -62,12 +53,4 @@ export default {
     text-align left
     margin .5em
 
-  .card-category__icon
-    font-size 1.3em
-    margin-bottom 0
-    margin-right .5em
-
-  .card-category__name
-    font-size 1em
-    display inline
 </style>
